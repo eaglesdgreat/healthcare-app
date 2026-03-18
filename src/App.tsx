ee3a465e58229209ef10dadf3baecd958b3c9a3b
Emmanuel <emmanuel.okponobi@zedcrest.com>
2026-03-18 16:58:02 +0100

    Setup for react query for data fetching and caching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import ThemeProvider from '@/context/theme/ThemeProvider'
 import { router } from '@/router'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
